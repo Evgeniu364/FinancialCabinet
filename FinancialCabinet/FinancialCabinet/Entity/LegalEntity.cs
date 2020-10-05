eb877ef58bd8e043e24383073c8aa867f480619c
boss <botviinikov@bk.ru>
2020-10-06 00:49:16 +0300

    Add Individual and LegalEntity

---
+++ FinancialCabinet/FinancialCabinet/Entity/LegalEntity.cs
@@ -0,0 +1,15 @@
+using System;
+using FinancialCabinet.Interface;
+
+namespace FinancialCabinet.Entity
+{
+    public class LegalEntity : ILegalEntity
+    {
+        public Guid Id { get; set; }
+        public string CompanyName { get; set; }
+        public int Unp { get; set; }
+        public int NumberDocument { get; set; }
+        public double CashTurnover { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ FinancialCabinet/FinancialCabinet/Interface/IIndividual.cs
@@ -0,0 +1,18 @@
+using System;
+using FinancialCabinet.Entity;
+
+namespace FinancialCabinet.Interface
+{
+    public interface IIndividual
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TypeDocument { get; set; }
+        public string NumberDocument { get; set; }
+        public double Salary { get; set; }
+        public User User { get; set; }
+    }
+}
