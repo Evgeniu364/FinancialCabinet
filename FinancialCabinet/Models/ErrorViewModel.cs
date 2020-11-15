1812b59eb74bcff3f4d8acae8ee83eb008fecaf5
Meleron <daniiltrotski487@gmail.com>
2020-11-16 00:05:08 +0300

    Add mvc project

---
+++ FinancialCabinet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FinancialCabinet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
