08eb779da2a907c0b6472258fa8d14486151a6af
Ilya <z7635332@gmail.com>
2020-11-17 02:01:40 +0300

    Add users.

---
+++ FinancialCabinet/Controllers/AccountController.cs
@@ -0,0 +1,153 @@
+﻿using FinancialCabinet.Entity;
+using FinancialCabinet.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialCabinet.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                Individual individual = new Individual();
+                LegalEntity legal = new LegalEntity();
+                if (model.IsIndividual)
+                {
+                    individual = new Individual { Id = Guid.NewGuid(), Name = model.Name, LastName = model.LastName, Patronymic = model.Patronymic, DateOfBirth = Convert.ToDateTime(model.DateOfBirth), TypeDocument = model.TypeDocument, NumberDocument = model.DocumentNumber, Salary = Convert.ToDouble(model.Salary) };
+                }
+                else
+                {
+                    legal = new LegalEntity { Id = Guid.NewGuid(), CompanyName = model.CompanyName, Unp = Convert.ToInt32(model.Unp), NumberDocument = Convert.ToInt32(model.NumberDocument), CashTurnover = Convert.ToDouble(model.CashTurnover) };
+                }
+
+                var user = new User { 
+                    UserName = model.Email,
+                    Phone = model.Phone,
+                    Email = model.Email,
+                    Address = model.Address,
+                    Individual = model.IsIndividual ? individual : null,
+                    LegalEntity = model.IsIndividual ? null : legal,
+                    IndividualID = model.IsIndividual ? individual.Id : Guid.Empty,
+                    LegalEntityID = model.IsIndividual ? Guid.Empty : legal.Id,
+                    EmailConfirmed = true };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToLocal(returnUrl);
+                }
+                if (result.IsLockedOut)
+                {
+                    return RedirectToAction(nameof(Lockout));
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Lockout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ FinancialCabinet/Controllers/ManageController.cs
@@ -0,0 +1,32 @@
+﻿using FinancialCabinet.Entity;
+using FinancialCabinet.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FinancialCabinet.Controllers
+{
+    public class ManageController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public ManageController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userManager.FindByNameAsync(User.Identity.Name));
+        }
+    }
+}
