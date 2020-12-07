01f264e38338d3629802aaaa349ac21f0abdc8de
Ilya <z7635332@gmail.com>
2020-12-08 00:14:29 +0300

    Add email confirmation.

---
+++ FinancialCabinet/Service/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+using MailKit.Security;
+
+namespace FinancialCabinet.Service
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "EMAIL"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.Auto);
+                await client.AuthenticateAsync("EMAIL", "PASSWORD");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
